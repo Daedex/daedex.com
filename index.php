70ce1b3a5bb9c5dad16447d0db124e06c321ae47
ma77ken7 <ma77ken7@gmail.com>
2015-08-30 10:35:46 -0400

    updates

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Daedex</title>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="bootstrap-3.3.5-dist/css/bootstrap.min.css">
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+		<link rel="stylesheet" href="style.css">
+		<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+		<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+	</head>
+
+	<body>
+	  	<div class="container">	  		
+	  		<div class="row">
+			  	<div class="col-md-12">
+				  	<h3 style="color:black; font-size:2em;">Dae<span style="color:grey;">dex</span></h3>						  							  	
+			  	</div>
+		  	</div>				  		  				
+			<div class="row">
+				<div class="col-md-2">
+				</div>
+
+
+				<div class="col-md-10">					
+					<div class="row">
+						<div class="container-fluid" id="selectionArea">
+							<ul class="nav nav-tabs">
+								<li class="active"><a data-toggle="tab" href="#resultsTab">Results</a></li>
+								<li><a data-toggle="tab" href="#outputsTab">Outputs</a></li>
+							</ul>
+							<div class="tab-content" style="padding-left:5px;padding-top:5px;">
+								<div id="resultsTab" class="tab-pane fade in active">
+									
+								</div>
+								<div id="outputsTab" class="tab-pane">
+																			
+								</div>
+							</div>
+						</div>				
+					</div> 
+				</div> 
+			</div> 			
+		</div>
+
+		<script type="text/javascript" src="master.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" type="text/javascript"></script>	
+  	
+ 	</body>
+</html>
